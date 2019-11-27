b95725d05dc3a55c273656197ee767b5cbf1d1ab
Libertonius <gorland1314@list.ru>
2019-11-27 21:56:46 +0300

    Revert "off debug mode"
    This reverts commit 01461d949da5ead9387928a36dd90c69d9144a73.

--- untitled/settings.py
+++ untitled/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'yn@g^s&hw8mm^#@n!nn8pmsur1s7w!3u2n3=r*y)=$&a6#l)ni'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -125,5 +125,3 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/')
 ]
-
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
